032581cff6e9ef160181a68212ed6565f6f0015d
beambot56 <46430236+beambot56@users.noreply.github.com>
2019-01-07 10:55:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NTE5MjY4OTUyOTU0NjM0MjUy.DxRCjA.ngXmDTdP1-YDl61HqBYnoqrroG8';
+exports.TOKEN = 'x';
 
 exports.PREFIX = '?';
 
-exports.GOOGLE_API_KEY = 'AIzaSyCWIJxEf8qUOlxXf1CqhZi6JGmNunTmzLI';
+exports.GOOGLE_API_KEY = 'x';
